e84507a6bdbe70c047d152307c0fa29b05de782b
Oleg <oleg.soldatikhine1@gmail.com>
2024-01-26 21:21:17 +0200

    start end game

---
+++ Narri/Assets/Scripts/Controllers/SceneController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController instance;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+       
+    }
+
+    public void ChangeScene(int index)
+    {
+        Debug.Log(index);
+        SceneManager.LoadScene(index);
+    }
+
+    public void exitgame()
+    {
+        
+        Application.Quit();
+    }
+
+}
